526486fb0399e097512abfb0ec4a8a6d4564b719
Fanil_Jr <ethic91@icloud.com>
2022-08-20 23:40:10 +0500

    Initial Commit

---
+++ NavigationVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationVK
+//
+//  Created by Fanil_Jr on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
